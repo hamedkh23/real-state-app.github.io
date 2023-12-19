77e4cf8097d9ee81d6d42b8f4799b0b75e413df9
hamed khodabakhsh <hamed.kh.2323@gmail.com>
2023-12-19 22:14:58 +0330

    little edit

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,6 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <h1>سلام</h1>
     </main>
   );
