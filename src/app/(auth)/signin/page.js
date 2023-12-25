bc5f4cc2a71de5c9a3d12ad286e89e186aeea03b
hamed khodabakhsh <hamed.kh.2323@gmail.com>
2023-12-25 19:55:10 +0330

    Authentication of signup page  and signin page

--- src/app/(auth)/signin/page.js
+++ src/app/(auth)/signin/page.js
@@ -1,7 +1,13 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 import SigninPage from "@/template/SigninPage";
 
-function page() {
+async function Signin() {
+  const session = await getServerSession(authOptions);
+  if (session) redirect("/");
+
   return <SigninPage />;
 }
 
-export default page;
+export default Signin;
--- src/app/(auth)/signup/page.js
+++ src/app/(auth)/signup/page.js
@@ -1,6 +1,12 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 import SignupPage from "@/template/SignupPage";
 
-function Signup() {
+async function Signup() {
+  const session = await getServerSession(authOptions);
+  if (session) redirect("/");
+
   return <SignupPage />;
 }
 
