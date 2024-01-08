cad1d9dee6788d0f3f4394f7e5a80c4ce3f90d6f
hamed khodabakhsh <hamed.kh.2323@gmail.com>
2024-01-08 17:47:28 +0330

    adminpage development

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -2,12 +2,22 @@ import DashboardSidebar from "@/layout/DashboardSidebar";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import connectDB from "@/utils/connectDB";
+import User from "@/models/User";
 
 async function DashboardLayout({ children }) {
   const session = await getServerSession(authOptions);
   if (!session) redirect("/signin");
 
-  return <DashboardSidebar>{children}</DashboardSidebar>;
+  await connectDB();
+  const user = await User.findOne({ email: session.user.email });
+  if (!user) return <h3>مشکلی پیش آمده است</h3>;
+
+  return (
+    <DashboardSidebar role={user.role} email={user.email}>
+      {children}
+    </DashboardSidebar>
+  );
 }
 
 export default DashboardLayout;
--- src/components/layout/DashboardSidebar.js
+++ src/components/layout/DashboardSidebar.js
@@ -1,22 +1,20 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import styles from "@/layout/DashboardSidebar.module.css";
 import LogoutButton from "@/module/LogoutButton";
-import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { CgProfile } from "react-icons/cg";
 
-async function DashboardSidebar({ children }) {
-  const session = await getServerSession(authOptions);
-
+async function DashboardSidebar({ children, email, role }) {
   return (
     <div className={styles.container}>
       <div className={styles.sidebar}>
         <CgProfile />
-        <p>{session?.user.email}</p>
+        {role === "ADMIN" ? "Admin" : null}
+        <p>{email}</p>
         <span></span>
         <Link href="/dashboard">حساب کاربری</Link>
         <Link href="/dashboard/my-profiles">آگهی های من</Link>
         <Link href="/dashboard/add">ثبت آگهی</Link>
+        {role === "ADMIN" ? <Link href="/admin">در انتظار تایید</Link> : null}
         <LogoutButton />
       </div>
       <div className={styles.main}>{children}</div>
