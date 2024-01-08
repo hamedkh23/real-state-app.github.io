79fa60d0f27a6c40d0413649e799abc5f2a476be
hamed khodabakhsh <hamed.kh.2323@gmail.com>
2024-01-08 18:17:44 +0330

    create meta tags

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -5,6 +5,10 @@ import { redirect } from "next/navigation";
 import connectDB from "@/utils/connectDB";
 import User from "@/models/User";
 
+export const metadata = {
+  title: "پنل کاربری املاک | پروژه بوتواستارت",
+};
+
 async function DashboardLayout({ children }) {
   const session = await getServerSession(authOptions);
   if (!session) redirect("/signin");
